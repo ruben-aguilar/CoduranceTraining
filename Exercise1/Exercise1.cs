144391cd196e4e4e9e52fe9f98e1626148b920dd
Ruben Aguilar Becerra <raguilarbecerra@vistaprint.com>
2018-05-10 10:03:32 +0200

    Initial commit

---
+++ Exercise1/Exercise1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Exercise1
+{
+    [TestFixture]
+    class Exercise1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
